2bb44e945fb52cca6fe1551d2e700aa9cb20df5f
Yuta Ueno <youbo0129ueno@gmail.com>
2026-07-08 00:09:36 +0900

    feat: 国語・英語・理科のサンプル教材を追加

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './themes/base.css'
 import './themes/social.css'
+import './themes/japanese.css'
+import './themes/english.css'
 import './themes/math.css'
+import './themes/science.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
